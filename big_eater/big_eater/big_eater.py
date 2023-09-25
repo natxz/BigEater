ea97511f56deced26a1d7541964eb15711b4aec4
Nathan Ndombasi <natrosity@Nathans-MacBook-Pro.local>
2023-09-25 21:40:05 +0100

    SDET-1: able to draw a circle

--- big_eater/big_eater/big_eater.py
+++ big_eater/big_eater/big_eater.py
@@ -3,14 +3,19 @@ import sys
 
 import pygame
 
+from big_eater_cursor import ClickableSprite
 
 # Environments 
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
+# Colours
 BLACK = (0, 0, 0)
 WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+BLUE = (0, 0, 100)
+
+# Window Details
+WINDOW_HEIGHT = 1000
+WINDOW_WIDTH = 1850
 
 class BigEater():
     def main():
@@ -20,14 +25,22 @@ class BigEater():
         CLOCK = pygame.time.Clock()
         SCREEN.fill(WHITE)
 
+        # cursor
+        # sprite = ClickableSprite(pygame.Surface((100, 100)), 50, 50, BigEater.on_click)
+        # group = pygame.sprite.GroupSingle(sprite=sprite)
+        # events =  pygame.event.get()
         while True:
             BigEater.drawGrid()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            pygame.display.update()
 
+                if event.type == pygame.MOUSEBUTTONUP:
+                    BigEater.drawCircle()
+                    pygame.display.update()
+            
+            pygame.display.update()
 
     def drawGrid():
         blockSize = 20 #Set the size of the grid block
@@ -35,6 +48,16 @@ class BigEater():
             for y in range(0, WINDOW_HEIGHT, blockSize):
                 rect = pygame.Rect(x, y, blockSize, blockSize)
                 pygame.draw.rect(SCREEN, BLACK, rect, 1)
+ 
+    def get_mouse_position():
+        position = pygame.mouse.get_pos()
+        return position
+
+    def drawCircle():
+        position=BigEater.get_mouse_position()
+        pygame.draw.circle(SCREEN, BLUE, position, 5)
+
+        
 
 
 if __name__ == "__main__":
---
+++ big_eater/big_eater/big_eater_cursor.py
@@ -0,0 +1,18 @@
+import pygame
+
+class ClickableSprite(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, callback):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.callback = callback
+
+    def update(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                if self.rect.collidepoint(event.pos):
+                    self.callback()
+    
+    
