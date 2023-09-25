dc218d58880528a716d3d40c3f4373c5b39fbb5a
Nathan Ndombasi <natrosity@Nathans-MacBook-Pro.local>
2023-09-25 18:26:59 +0100

    SDET-1: box conjured

--- big_eater/big_eater/big_eater.py
+++ big_eater/big_eater/big_eater.py
@@ -1,10 +1,41 @@
 from os import environ
+import sys
+
 import pygame
 
 
 # Environments 
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 400
 
 class BigEater():
-    print("everything works")
+    def main():
+        global SCREEN, CLOCK
+        pygame.init()
+        SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        CLOCK = pygame.time.Clock()
+        SCREEN.fill(WHITE)
+
+        while True:
+            BigEater.drawGrid()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+
+
+    def drawGrid():
+        blockSize = 20 #Set the size of the grid block
+        for x in range(0, WINDOW_WIDTH, blockSize):
+            for y in range(0, WINDOW_HEIGHT, blockSize):
+                rect = pygame.Rect(x, y, blockSize, blockSize)
+                pygame.draw.rect(SCREEN, BLACK, rect, 1)
+
+
+if __name__ == "__main__":
+    BigEater.main()
